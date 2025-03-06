6e533e3571f6b9f47e9c3f1981d56efc1a4d1562
Ramesh Kumar <test@youremail.com>
2025-03-06 23:16:41 +0530

    Updated API URLs for Render deployment (backend)

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,7 +15,7 @@ const __dirname = path.resolve();
 
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: "https://mern-chat-app-2-p6d6.onrender.com",
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE"],
         allowedHeaders: ["Content-Type"],
