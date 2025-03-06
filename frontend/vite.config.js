1230f8dbced3d69ab1529f1534cc546ac7f321e7
Ramesh Kumar <test@youremail.com>
2025-03-06 23:17:47 +0530

    Updated API URLs for Render deployment (frontend)

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     server: {
         proxy: {
             "/api": {
-                target: "http://localhost:5000",
+                target: "https://mern-chat-app-1-lowr.onrender.com",
                 changeOrigin: true,
                 rewrite: (path) => path.replace(/^\/api/, ""),
             },
