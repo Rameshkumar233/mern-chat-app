5994f5cd37cdc8dab68f6cfaa78bdac93df972ae
Ramesh Kumar <test@youremail.com>
2025-03-07 13:19:54 +0530

    Fix the URL

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? import.meta.env.VITE_API_URL : "/api",
+    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "https://mern-chat-app-1-lowr.onrender.com/api",
     withCredentials: true,
 });
