98f37c655853a57684ea6a4fffdd893c90eaa556
Ramesh Kumar <test@youremail.com>
2025-09-27 21:32:29 +0530

    refactor: Replace nodmailer with Brevo API (SMTP blocked on render)

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -1,36 +1,42 @@
-import transporter from "../config/email.config.js";
+import  brevo  from "@getbrevo/brevo"
 import { verificationEmailTemplate, welcomeEmailTemplate } from "./emailTemplate.js";
 
-export const sendVerificationEmail = async (to, verificationToken) => {
-    let recipient = [to];
-    try {
-        let response = await transporter.sendMail({
-            from: `"Chat app" <${process.env.EMAIL_USER || "no-reply@chatone-app.com"}>`,
-            to: recipient,
-            subject: "Verify Email Address",
-            html: verificationEmailTemplate(verificationToken),
-        });
+const isDev = process.env.NODE_ENV === "development";
 
-        console.log("Email sent successfully", response.envelope);
+const apiInstance = new brevo.TransactionalEmailsApi()
+const apiKey = apiInstance.authentications["apiKey"]
+apiKey.apiKey = process.env.BREVO_API_KEY
+
+
+export const sendVerificationEmail = async (to, verificationToken) => {
+    const emailData = {
+        sender: {email : process.env.EMAIL_USER},
+        to: [{email: isDev ? process.env.EMAIL_USER : to}],
+        subject: "Verify Email Address",
+        htmlContent: verificationEmailTemplate(verificationToken)
+    }
+
+    try {
+        const info = await apiInstance.sendTransacEmail(emailData);
+        console.log("Verify Email sent successfully", info.body);
     } catch (error) {
-        console.error("Error in sending verification email", error);
-        throw new Error(`Error in sending verification email ${error}`);
+        console.error("Error sending verification email", error);
+        throw new Error(`Error sending verification email ${error}`);
     }
 };
 
 export const sendWelcomeEmail = async (to, username) => {
-    let recipient = [to];
+    const emailData = {
+        sender: {email : process.env.EMAIL_USER},
+        to: [{email: isDev ? process.env.EMAIL_USER : to}],
+        subject: "Welcome to Chat App",
+        htmlContent: welcomeEmailTemplate(username)
+    }
     try {
-        let response = await transporter.sendMail({
-            from: `"Chat app" <${process.env.EMAIL_USER || "no-reply@chatone-app.com"}> `,
-            to: recipient,
-            subject: "Welcome to Chat One app",
-            html: welcomeEmailTemplate(username),
-        });
-
-        console.log("Welcome Email sent successfully", response.envelope);
+        const info = await apiInstance.sendTransacEmail(emailData);
+        console.log("Welcome Email sent successfully", info.body);
     } catch (error) {
-        console.error("Error in sending welcome email", error);
-        throw new Error(`Error in sending welcome email ${error}`);
+        console.error("Error sending welcome email", error);
+        throw new Error(`Error sending welcome email ${error}`);
     }
 };
